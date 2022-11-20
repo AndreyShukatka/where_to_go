d6920d9b021ae03fede1f05d7eeaba79eb4c0405
AndreyShukatka <9080106@gmail.com>
2022-11-21 01:11:29 +0300

    Add Place, Image models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Place, Image
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
--- places/models.py
+++ places/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+# from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Place(models.Model):
+    objects = None
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description_short = models.CharField(max_length=350, verbose_name='Краткое описание')
+    # description_long = HTMLField(verbose_name='Полное описание')
+    lat = models.FloatField(null=True, verbose_name='Точка координат lat')
+    lng = models.FloatField(null=True, verbose_name='Точка координат lng')
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    objects = None
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Место',
+        related_name='image'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        blank=True,
+        upload_to=''
+    )
+    position = models.IntegerField(
+        default=0,
+        verbose_name='позиция'
+    )
+
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return f'{self.position} {self.place.title}'
