262812f1f2fe7cd88757320860e21118dd0d7a7e
AndreyShukatka <9080106@gmail.com>
2022-11-21 00:44:38 +0300

    Add frontend

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from django.conf.urls.static import static
+from django.conf import settings
 
 def index(request):
     context = {}
@@ -25,4 +27,4 @@ def index(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
