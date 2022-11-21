df51d2452385f0f9723f4c500839035e0c53ee84
AndreyShukatka <9080106@gmail.com>
2022-11-21 22:58:45 +0300

    Высушил код

--- places/models.py
+++ places/models.py
@@ -3,7 +3,6 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    objects = None
     title = models.CharField(max_length=200, verbose_name='Название')
     description_short = models.CharField(max_length=350, verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Полное описание')
@@ -15,7 +14,6 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    objects = None
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
