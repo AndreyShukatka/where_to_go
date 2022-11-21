bd806292a47c236f5ef02711a9c3816a4beea77c
AndreyShukatka <9080106@gmail.com>
2022-11-21 23:00:22 +0300

    Исправил поле

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
-    description_short = models.CharField(max_length=350, verbose_name='Краткое описание')
+    description_short = models.TextField(verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Полное описание')
     lat = models.FloatField(null=True, verbose_name='Точка координат lat')
     lng = models.FloatField(null=True, verbose_name='Точка координат lng')
