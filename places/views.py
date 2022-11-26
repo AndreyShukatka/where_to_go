deb9d0190de95f4d40e01e8769e4f1208bcd72b8
AndreyShukatka <9080106@gmail.com>
2022-11-26 16:06:16 +0300

    Убрал лишнее

--- places/views.py
+++ places/views.py
@@ -1,12 +1,8 @@
-import json
-
 from django.shortcuts import render
-from .models import Place, Image
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
+from .models import Place
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.conf import settings
 
 
 # Create your views here.
